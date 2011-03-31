a50435165339dda9e22e605de771f81861aa9026
Deon Garrett <deong@acm.org>
2011-03-31 01:50:46 +0000

    updated tests

--- test/com/cataclysmicmutation/words/test_core.clj
+++ test/com/cataclysmicmutation/words/test_core.clj
@@ -1,3 +1,12 @@
 (ns com.cataclysmicmutation.words.test-core
-  (:use com.cataclysmicmutation.words.core clojure.test))
+  (:use com.cataclysmicmutation.words.core
+        com.cataclysmicmutation.words.trie
+        clojure.test))
 
+(deftest test-find-valid-words
+  (let [dict (load-word-file "res/scrabble_dict.txt")]
+    (is (= (count (find-valid-words dict "*X**" "AEIRSTL")) 6))
+    (is (= (count (find-valid-words dict "APP***" "AEIRSTL")) 7))
+    (is (= (count (find-valid-words dict "Q***" "AOTIDUP")) 10))
+    (is (= (count (find-valid-words dict "Q***" "AOTIDZP")) 2))
+    (is (= (find-valid-words dict "Q****" "AOTIDZP") []))))
